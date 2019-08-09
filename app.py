2d7f5f6b0f1322dd6d4791c281e402af1bb3492c
Ubuntu <ubuntu@ip-172-31-18-47.eu-west-1.compute.internal>
2019-08-09 16:13:38 +0000

    added jumbotron image, added  dynamic data from mongodb to cards and modal content

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import config
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
@@ -15,7 +14,13 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def meal_types():
-    return render_template("test_connection.html", meals=mongo.db.meal_type.find())
+    return render_template("test_connection.html", 
+    most_liked=mongo.db.meal_type.find(),
+    favourites=mongo.db.meal_type.find(),
+    meal_type_category=mongo.db.meal_type.find(),
+    quisine_type_category=mongo.db.meal_type.find(),
+    special_diet_type_category=mongo.db.meal_type.find(),
+    difficulty_type_category=mongo.db.meal_type.find())
 
 
 if __name__ == '__main__':
