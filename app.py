e2a02760a8c70bba7964865c4108536d330a25ec
Ubuntu <ubuntu@ip-172-31-18-47.eu-west-1.compute.internal>
2019-08-16 12:47:55 +0000

    Added Sign In functionality and Started code to display specific recipe when clicked

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+import bcrypt
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
@@ -11,20 +12,132 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:MyRootDataBase@myfirstcluster-ur0q
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 def meal_types():
+    if 'username' in session:
+        return render_template("home.html", 
+        most_liked=mongo.db.meal_type.find(),
+        favourites=mongo.db.meal_type.find(),
+        meal_type_category=mongo.db.meal_type.find(),
+        cuisine_type_category=mongo.db.meal_type.find(),
+        special_diet_type_category=mongo.db.meal_type.find(),
+        difficulty_type_category=mongo.db.meal_type.find(),
+        username=mongo.db.users.find_one({"username": session['username']}))
+     
+    return render_template('register_login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+    if existing_user is None:
+       hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+       users.insert({'name' : request.form['username'], 'password' : hashpass})
+       session['username'] = request.form['username']
+       return render_template("home.html", 
+       most_liked=mongo.db.meal_type.find(),
+       favourites=mongo.db.meal_type.find(),
+       meal_type_category=mongo.db.meal_type.find(),
+       cuisine_type_category=mongo.db.meal_type.find(),
+       special_diet_type_category=mongo.db.meal_type.find(),
+       difficulty_type_category=mongo.db.meal_type.find(),
+       username=mongo.db.users.find_one({"username": session['username']}))
+        
+    return render_template('register_login.html',
+    flash=flash('That Username is already taken please try again!'))    
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']: 
+            session['username'] = request.form['username']
+            return render_template("home.html", 
+            most_liked=mongo.db.meal_type.find(),
+            favourites=mongo.db.meal_type.find(),
+            meal_type_category=mongo.db.meal_type.find(),
+            cuisine_type_category=mongo.db.meal_type.find(),
+            special_diet_type_category=mongo.db.meal_type.find(),
+            difficulty_type_category=mongo.db.meal_type.find(),
+            username=mongo.db.users.find_one({"username": session['username']}))
+
+        return render_template('register_login.html',
+        flash=flash('Wrong Username and Password Combination. Please Try Again!'))
+
     return render_template("home.html", 
     most_liked=mongo.db.meal_type.find(),
     favourites=mongo.db.meal_type.find(),
     meal_type_category=mongo.db.meal_type.find(),
     cuisine_type_category=mongo.db.meal_type.find(),
     special_diet_type_category=mongo.db.meal_type.find(),
-    difficulty_type_category=mongo.db.meal_type.find())
+    difficulty_type_category=mongo.db.meal_type.find(),
+    username=mongo.db.users.find_one({"username": session['username']}))   
+
+@app.route('/guest_login')
+def guest_login():
+    if 'username' in session:
+        return render_template("home.html", 
+        most_liked=mongo.db.meal_type.find(),
+        favourites=mongo.db.meal_type.find(),
+        meal_type_category=mongo.db.meal_type.find(),
+        cuisine_type_category=mongo.db.meal_type.find(),
+        special_diet_type_category=mongo.db.meal_type.find(),
+        difficulty_type_category=mongo.db.meal_type.find(),
+        username=mongo.db.users.find_one({"username": session['username']}))
+    
+    return render_template("home.html", 
+        most_liked=mongo.db.meal_type.find(),
+        favourites=mongo.db.meal_type.find(),
+        meal_type_category=mongo.db.meal_type.find(),
+        cuisine_type_category=mongo.db.meal_type.find(),
+        special_diet_type_category=mongo.db.meal_type.find(),
+        difficulty_type_category=mongo.db.meal_type.find())    
+
+
+@app.route('/edit_test')
+def edit_test():
+    return render_template("editbuttontest.html",
+    meal_type_category=mongo.db.meal_type.find())
+
+@app.route('/edit_recipe/<meal_type>')
+def edit_recipe(meal_type):
+    the_task =  mongo.db.meal_type.find_one({"_id": ObjectId(meal_type)})
+    all_categories =  mongo.db.meal_type.find()
+    return render_template('editrecipe.html', task=the_task,
+                           categories=all_categories,
+    username=mongo.db.users.find_one({"username": session['username']}))                       
     
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("addrecipe.html")
+    if 'username' in session:
+        return render_template("addrecipe.html",
+        username=mongo.db.users.find_one({"username": session['username']}))
+        
+    return render_template('register_login.html',
+           flash=flash('You must Sign In to Add a Recipe!'))
+    
+    
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    meals=mongo.db.meal_type
+    meals.insert_one(request.form.to_dict())
+    return render_template('addrecipe.html',
+           flash=flash('Your Recipe Has Been Added!'),
+           username=mongo.db.users.find_one({"username": session['username']}))
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('register_login.html',
+        flash=flash('You have been Signed Out!'))
+       
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host='0.0.0.0', port=8080, debug=True)
+    
+    
---
+++ static/media/form.js
@@ -0,0 +1,24 @@
+$(function () {
+ for(var i=1; i<=120; i++){
+   var select = document.getElementById("prep_time");
+    var option = document.createElement("OPTION");
+	    select.options.add(option);
+	    option.text = i;
+	    option.value = i;
+}
+  for(var i=5; i<=300; i+= 5){
+   var select = document.getElementById("cook_time");
+    var option = document.createElement("OPTION");
+	    select.options.add(option);
+	    option.text = i;
+	    option.value = i;
+}
+  for(var i=1; i<=10; i+= 1){
+   var select = document.getElementById("servings");
+    var option = document.createElement("OPTION");
+	    select.options.add(option);
+	    option.text = i;
+	    option.value = i;
+}
+
+});
