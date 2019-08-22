aa06669c6aa66670a0506cb02afe7a4b918696f7
Ubuntu <ubuntu@ip-172-31-18-47.eu-west-1.compute.internal>
2019-08-22 20:04:23 +0000

    Modified the layout of the add recipe page, still have the dynamic buttons to work on

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from bson.objectid import ObjectId
 from flask_mail import Mail, Message
 from app_variables import *
 
-app = Flask(__name__)
+# Configure Flask and Mail Server #
+app = Flask(__name__, static_url_path='/static')
 app.config["MONGO_DBNAME"] = MONGO_DBNAME
 app.config["MONGO_URI"] = MONGO_URI
 
@@ -26,6 +27,32 @@ app.config['MAIL_ASCII_ATTACHMENTS'] = MAIL_ASCII_ATTACHMENTS
 mail = Mail(app)
 mongo = PyMongo(app)
 
+# Global Variables used multiple Times#
+# Get Unique and Specific Data from each Key (Used in home.html) #
+meal_types_category = mongo.db.meal_type.distinct('meal_type')
+cuisine_type_categories = mongo.db.meal_type.distinct('cuisine_type')
+special_diet_type_category = mongo.db.meal_type.distinct('special_diet')
+difficulty_type_category = mongo.db.meal_type.distinct('difficulty')
+
+# Homepage will render home.html if user logged in or register.html if not#
+@app.route('/')
+def meal_types():
+    # If the user is logged in #
+    if 'username' in session:
+        return render_template("home.html", 
+        most_liked= mongo.db.meal_type.find(),
+        favourites= mongo.db.meal_type.find(),
+        meal_type_category= meal_types_category,
+        cuisine_type_category= cuisine_type_categories,
+        special_diet_type_category= special_diet_type_category,
+        difficulty_type_category= difficulty_type_category,
+        username= mongo.db.users.find_one({"username": session['username']}))
+        
+    # Else send them to the register.html page# 
+    return render_template('register_login.html')
+
+
+# Contact Us Form and Function for Sending the email#
 @app.route('/contact_us', methods=['POST', 'GET'])
 def contact_us():
     return render_template('contact_us.html')
@@ -40,33 +67,18 @@ def send_email():
     
     mail.send(email)
     return render_template('contact_us.html',
-    flash=flash('Your Email Has been Sent!'))
+    flash= flash('Your Email Has been Sent!'))
 
-@app.route('/')
-def meal_types():
-    meal_types_category = mongo.db.meal_type.distinct('meal_type')
-    cuisine_type_categories = mongo.db.meal_type.distinct('cuisine_type')
-    special_diet_type_category = mongo.db.meal_type.distinct('special_diet')
-    difficulty_type_category = mongo.db.meal_type.distinct('difficulty')
-    if 'username' in session:
-        return render_template("home.html", 
-        most_liked=mongo.db.meal_type.find(),
-        favourites=mongo.db.meal_type.find(),
-        meal_type_category= meal_types_category,
-        cuisine_type_category=cuisine_type_categories,
-        special_diet_type_category=special_diet_type_category,
-        difficulty_type_category=difficulty_type_category,
-        username=mongo.db.users.find_one({"username": session['username']}))
-        
-     
-    return render_template('register_login.html')
 
+# Register Page, Has Sign Up (/register), Log In(/login) or Continue as Guest Options(/guest_login)#
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    # Step 1: Check to see if username exists already #
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'name' : request.form['username']})
 
+    # Step 2: If Username doesn't exist add their details to the database and hash their password #
     if existing_user is None:
        hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
        users.insert({'name' : request.form['username'], 'password' : hashpass, 'email' : request.form['email']})
@@ -74,24 +86,23 @@ def register():
        return render_template("home.html", 
        most_liked=mongo.db.meal_type.find(),
        favourites=mongo.db.meal_type.find(),
-       meal_type_category=mongo.db.meal_type.find(),
-       cuisine_type_category=mongo.db.meal_type.find(),
-       special_diet_type_category=mongo.db.meal_type.find(),
-       difficulty_type_category=mongo.db.meal_type.find(),
+       meal_type_category= meal_types_category,
+       cuisine_type_category=cuisine_type_categories,
+       special_diet_type_category=special_diet_type_category,
+       difficulty_type_category=difficulty_type_category,
        username=mongo.db.users.find_one({"username": session['username']}))
-        
+    
+    # Step 3: Else Return the user to register page with error #    
     return render_template('register_login.html',
     flash=flash('That Username is already taken please try again!'))    
 
+# Register Page, Login section#
 @app.route('/login', methods=['POST'])
 def login():
+    # Get the details the user has entered #
     users = mongo.db.users
     login_user = users.find_one({'name' : request.form['username']})
-    meal_types_category = mongo.db.meal_type.distinct('meal_type')
-    cuisine_type_categories = mongo.db.meal_type.distinct('cuisine_type')
-    special_diet_type_category = mongo.db.meal_type.distinct('special_diet')
-    difficulty_type_category = mongo.db.meal_type.distinct('difficulty')
-
+    # If the username is in the database check the password and go to the homepage#
     if login_user:
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']: 
             session['username'] = request.form['username']
@@ -103,26 +114,16 @@ def login():
             special_diet_type_category=special_diet_type_category,
             difficulty_type_category=difficulty_type_category,
             username=mongo.db.users.find_one({"username": session['username']}))
-
+        # Else return to register screen with error if password incorrect#
         return render_template('register_login.html',
         flash=flash('Wrong Username and Password Combination. Please Try Again!'))
-
-    return render_template("home.html", 
-    most_liked=mongo.db.meal_type.find(),
-    favourites=mongo.db.meal_type.find(),
-    meal_type_category=mongo.db.meal_type.find(),
-    cuisine_type_category=mongo.db.meal_type.find(),
-    special_diet_type_category=mongo.db.meal_type.find(),
-    difficulty_type_category=mongo.db.meal_type.find(),
-    username=mongo.db.users.find_one({"username": session['username']}))   
+    # Else return to register screen with error if username incorrect#
+    return render_template('register_login.html',
+    flash=flash('Wrong Username and Password Combination. Please Try Again!'))   
 
 @app.route('/guest_login')
 def guest_login():
-    meal_types_category = mongo.db.meal_type.distinct('meal_type')
-    cuisine_type_categories = mongo.db.meal_type.distinct('cuisine_type')
-    special_diet_type_category = mongo.db.meal_type.distinct('special_diet')
-    difficulty_type_category = mongo.db.meal_type.distinct('difficulty')
-    
+    # If User is already logged in #
     if 'username' in session:
         return render_template("home.html", 
         most_liked=mongo.db.meal_type.find(),
@@ -133,6 +134,7 @@ def guest_login():
         difficulty_type_category=difficulty_type_category,
         username=mongo.db.users.find_one({"username": session['username']}))
     
+    # Else Render the same template as the above without a session cookie#
     return render_template("home.html", 
         most_liked=mongo.db.meal_type.find(),
         favourites=mongo.db.meal_type.find(),
@@ -142,17 +144,23 @@ def guest_login():
         difficulty_type_category=difficulty_type_category)    
 
 
+# When a 'Get Recipe' button is clicked, it queries the database for the specific recipe details and returns it to the user in recipepage.html #
 @app.route('/view_recipe/<search>', methods=['POST', 'GET'])
 def view_recipe(search):
     title_results =  mongo.db.meal_type.find_one( {'_id': ObjectId(request.form['get_recipe']) })
-    return render_template('recipepage.html', task=title_results,
-    username=mongo.db.users.find_one({"username": session['username']})) 
+    if 'username' in session:
+        return render_template('recipepage.html', task=title_results,
+        username=mongo.db.users.find_one({"username": session['username']}))
+    return render_template('recipepage.html', task=title_results)   
 
+# move this to the view recipe page so that it can be edited #
 @app.route('/edit_test')
 def edit_test():
     return render_template("editbuttontest.html",
     meal_type_category=mongo.db.meal_type.find())
 
+# Get's the _id of the recipe's edit button, queries the data base for the specific recipe details &
+# renders the editrecipe.html page with the recipe data for updating #
 @app.route('/edit_recipe/<meal_type>')
 def edit_recipe(meal_type):
     the_task =  mongo.db.meal_type.find_one({"_id": ObjectId(meal_type)})
@@ -160,26 +168,29 @@ def edit_recipe(meal_type):
     return render_template('editrecipe.html', task=the_task,
                            categories=all_categories,
     username=mongo.db.users.find_one({"username": session['username']}))  
-    
+
+# Search the database for matching word/s in the Meal Title, Prep Instructions & Cooking Instruction Values  #    
 @app.route('/search', methods=['POST', 'GET'])
 def search():
     search = request.form['search']
     title_results =  mongo.db.meal_type.find( { '$or': [ {'meal_title': { '$regex': search,  '$options': 'i' }}, 
                                               {'email_address': { '$regex': search, '$options': 'i' }} ] })
     return render_template('searchtest.html', task=title_results)
-    
+
+# If User is Logged In then allow them to add a Recipe #    
 @app.route('/add_recipe')
 def add_recipe():
     if 'username' in session:
         return render_template("addrecipe.html",
-        meal_type=mongo.db.meal_types.find(),
-        cuisine_types=mongo.db.cuisine_types.find(),
+        meal_types= mongo.db.meal_types.find(),
+        cuisine_types= mongo.db.cuisine_types.find(),
+        special_diet= mongo.db.special_diets.find(),
         username=mongo.db.users.find_one({"username": session['username']}))
-        
+# Else send them to the register page with message #        
     return render_template('register_login.html',
            flash=flash('You must Sign In to Add a Recipe!'))
     
-    
+# Function for inserting the recipe to the database, returns the to the page with success message #    
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     meals=mongo.db.meal_type
@@ -187,13 +198,18 @@ def insert_recipe():
     return render_template('addrecipe.html',
            flash=flash('Your Recipe Has Been Added!'),
            username=mongo.db.users.find_one({"username": session['username']}))
-    
+
+# If Logout Button is clicked then it removes the session cookie and brings them to the register page with message #
 @app.route('/logout')
 def logout():
     session.clear()
     return render_template('register_login.html',
         flash=flash('You have been Signed Out!'))
-        
+
+# Modal buttons on Hompage, #
+# Queries database and shows in Modal only the Unique values if that particlular Key #
+# If a value such as "Gluten-free" is not a value for a recipe it will not be shown in the list #
+# When value in modal is choosen it will display a list of recipes that contain that value#
 @app.route('/category_results', methods=['POST', 'GET'])
 def category_results():
     if request.form['type_category_button'] in ('Breakfast', 'Lunch', 'Dinner', 'Snack'):
@@ -202,7 +218,7 @@ def category_results():
     elif request.form['type_category_button'] in ('American', 'Chinese', 'French', 'Greek', 'Indian', 'Irish', 'Italian', 'Mexican', 'Thai', 'Turkish', 'Other'):
          category_results=mongo.db.meal_type.find({'cuisine_type': request.form['type_category_button']})
          return render_template('category_results.html', tasks=category_results)
-    elif request.form['type_category_button'] in ('Vegetarian', 'Vegan', 'Weight Watchers', 'Gluten-free', 'Ketogenic', 'High-protein', 'Low Fat', 'Low-Carb', 'Other Diet'):
+    elif request.form['type_category_button'] in ('Vegetarian', 'Vegan', 'Weight Watchers', 'Gluten-free', 'Ketogenic', 'High-protein', 'Low Fat', 'Low-Carb', 'Other Diet', 'Not Applicable'):
          category_results=mongo.db.meal_type.find({'special_diet': request.form['type_category_button']})
          return render_template('category_results.html', tasks=category_results)
     elif request.form['type_category_button'] in ('Easy', 'Medium', 'Hard'):
--- static/js/clone-form-td.js
+++ static/js/clone-form-td.js
@@ -3,50 +3,37 @@ $(function () {
         var num     = $('.clonedInput_1').length, // Checks to see how many "duplicatable" input fields we currently have
             newNum  = new Number(num + 1),      // The numeric ID of the new input field being added, increasing by 1 each time
             newElem = $('#entry' + num).clone().attr('id', 'entry' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
-    
-    /*  This is where we manipulate the name/id values of the input inside the new, cloned element
-        Below are examples of what forms elements you can clone, but not the only ones.
-        There are 2 basic structures below: one for an H2, and one for form elements.
-        To make more, you can copy the one for form elements and simply update the classes for its label and input.
-        Keep in mind that the .val() method is what clears the element when it gets cloned. Radio and checkboxes need .val([]) instead of .val('').
-    */
-        // Label for email field
-        newElem.find('.label_instructions').attr('id', 'ID' + newNum + '_reference').attr('name', 'ID' + newNum + '_reference').html('Step #' + newNum);
 
-        // Email input - text
-        newElem.find('.label_instructions').attr('for', 'ID' + newNum + '_email_address');
-        newElem.find('.input_instructions').attr('id', 'ID' + newNum + '_email_address').attr('name', 'ID' + newNum + '_email_address').val('');
+        // Label for input_ingredients field
+        newElem.find('.ingredients_label').attr('id', 'Field' + newNum + '_reference').attr('name', 'Field' + newNum + '_reference').html('Step #' + newNum);
+
+        // input_ingredients input - text
+        newElem.find('.label_ingredients').attr('id', 'Field' + newNum + '_input_ingredients').attr('name', 'Field' + newNum + '_input_ingredients').val=("");
 
     // Insert the new element after the last "duplicatable" input field
         $('#entry' + num).after(newElem);
-        $('#ID' + newNum + '_title').focus();
+        $('input[id=Field' + newNum +'_input_ingredients]').val('');
+        $('#Field' + newNum + '_title').focus();
+        
 
-    // Enable the "remove" button. This only shows once you have a duplicated section.
+    // Enables the "remove" button. This only shows once you have a duplicated section.
         $('#btnDel_1').attr('disabled', false);
 
-    // Right now you can only add 4 sections, for a total of 5. Change '5' below to the max number of sections you want to allow.
-        // This first if statement is for forms using input type="button" (see older demo). Delete if using button element.
-        if (newNum == 5)
+        // How many Fields are allowed
+        if (newNum == 10)
         $('#btnAdd_1').attr('disabled', true).prop('value', "You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached
-        // This second if statement is for forms using the new button tag (see Bootstrap demo). Delete if using input type="button" element.
-        if (newNum == 5)
-        $('#btnAdd_1').attr('disabled', true).text("You've reached the limit"); // value here updates the text in the 'add' button when the limit is reached 
     });
 
     $('#btnDel_1').click(function () {
     // Confirmation dialog box. Works on all desktop browsers and iPhone.
-        if (confirm("Are you sure you wish to remove this email? This cannot be undone."))
+        if (confirm("Are you sure you wish to remove this field? This cannot be undone."))
             {
                 var num = $('.clonedInput_1').length;
                 // how many "duplicatable" input fields we currently have
                 $('#entry' + num).slideUp('slow', function () {$(this).remove();
                 // if only one element remains, disable the "remove" button
                     if (num -1 === 1)
-                $('#btnDel_1').attr('disabled', true);
-                // enable the "add" button. IMPORTANT: only for forms using input type="button" (see older demo). DELETE if using button element.
-                $('#btnAdd_1').attr('disabled', false).prop('value', "Add email");
-                // enable the "add" button. IMPORTANT: only for forms using the new button tag (see Bootstrap demo). DELETE if using input type="button" element.
-                $('#btnAdd_1').attr('disabled', false).text("Add email");});
+                $('#btnDel_1').attr('disabled', true);});
             }
         return false; // Removes the last section you added
     });
